b220bf19692d4159d126423d3203a7f81af1d27c
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2021-09-01 23:45:01 +0200

    Testfiles Prepared

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,12 +1,9 @@
-// describe("index checks", () => 
-// {
-//     it("index is doing", () => 
-//     {
-//         console.log("working")
-//     });
+// describe('index checks', () => {
+//   it('index is doing', () => {
+//     console.log('working');
+//   });
 
-//     it("second test", () => 
-//     {
-//         console.log("and still going strong")
-//     })
-// })
+//   it('second test', () => {
+//     console.log('and still going strong');
+//   });
+// });
